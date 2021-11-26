fc943938b7fd14114a036c7d23addda564bada0a
SamDoggett <sdoggett@awarex.com>
2021-11-26 12:45:40 -0500

    Initial Commit

---
+++ Fake-Product-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fake-Product-App
+//
+//  Created by Sam Doggett on 11/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
